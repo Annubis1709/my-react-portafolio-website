a327f34d07db7763403f6984ef88b068aa277d39
Edier Sanchez Sanchez <sansankind@gmail.com>
2022-08-17 15:20:35 -0500

    useSate for active navBar is added.

--- src/components/nav/NavBar.jsx
+++ src/components/nav/NavBar.jsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navBar.css';
 import { ImHome, ImUser, ImBook } from 'react-icons/im';
 import {RiServiceFill} from "react-icons/ri";
 import {SiGooglemessages} from "react-icons/si";
 
 const NavBar = () => {
+    const [activeNav, setActiveNav] = useState('#');
     return (
         <nav >
-            <a href={"#"}  className='active'><ImHome /></a>
-            <a href={"#about"}><ImUser /></a>
-            <a href={"#experience"}><ImBook /></a>
-            <a href={"#services"}><RiServiceFill /></a>
-            <a href={"#contact"}><SiGooglemessages /></a>
+            <a
+                href={"#"}
+                onClick={ () => setActiveNav('#') }
+                className={ activeNav === '#' ? 'active': '' }>
+                <ImHome />
+            </a>
+            <a
+                href={"#about"}
+                onClick={ () => setActiveNav('#about') }
+                className={ activeNav === '#about' ? 'active': '' } >
+                <ImUser />
+            </a>
+            <a
+                href={"#experience"}
+                onClick={ () => setActiveNav('#experience') }
+                className={ activeNav === '#experience' ? 'active': '' } >
+                <ImBook />
+            </a>
+            <a
+                href={"#services"}
+                onClick={ () => setActiveNav('#services') }
+                className={ activeNav === '#services' ? 'active': '' } >
+                <RiServiceFill />
+            </a>
+            <a
+                href={"#contact"}
+                onClick={ () => setActiveNav('#contact') }
+                className={ activeNav === '#contact' ? 'active': '' } >
+                <SiGooglemessages />
+            </a>
         </nav>
     );
 };
